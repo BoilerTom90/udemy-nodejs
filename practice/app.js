dba03f9a9a2c5a45ed799260a5c22cfbb38f46c5
Tom Hoffman <thomas.hoffman@infinite.com>
2019-10-06 14:28:26 -0500

    Using util.inhertis and inheriting from the EventEmitter

--- practice/app.js
+++ practice/app.js
@@ -1,16 +1,24 @@
-// object.create examle
-// setting up the prototype chain
+var EventEmitter = require('events');
+var util = require('util');
 
-var person = {
-   fname: '',
-   lname: '',
-   greet: function () {
-      return this.fname + " " + this.lname;
-   }
+
+function Greetr() {
+   this.greeting = "Hello World";
 }
 
-var john = Object.create(person);
-john.fname = "John";
-john.lname = "Doe";
+// modify Greetr so that it has all the methods and properties of EventEmitter
+// this must be done before adding to the prototype with other properties/methods.
+util.inherits(Greetr, EventEmitter);
 
-console.log(john.greet());
+Greetr.prototype.greet = function (data) {
+   console.log(this.greeting + ": " + data);
+   this.emit('greet', data); // a property from EventEmitter;
+}
+
+var g1 = new Greetr();
+
+g1.on('greet', function (data) {
+   console.log("Someone greeted me with data: " + data);
+});
+
+g1.greet("fubar");
