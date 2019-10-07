420caed611b566f75e1318fbd3f9ff0fa8eb9240
Tom Hoffman <thomas.hoffman@infinite.com>
2019-10-06 21:29:04 -0500

    callback examples

--- practice/app.js
+++ practice/app.js
@@ -1,20 +1,11 @@
+function greet(callback) {
+   console.log("hello");
+   var data = {
+      name: 'John Doe'
+   };
+   callback(data);
+}
 
-
-// buffer is part of nodejs... require not necessary
-//var buffer = new Buffer('Hello', 'utf8');
-var buffer = new Buffer.from('Hello');
-console.log(buffer);
-console.log(buffer.toString());
-console.log(buffer.toJSON());
-buffer.write('wo');
-console.log(buffer.toString());
-
-// how to deal with binary data in a buffer
-// 
-var ab = new ArrayBuffer(8 /* bytes */);
-var view = new Int32Array(ab); // big enough for two values in ab
-view[0] = 5;
-view[1] = 15;
-view[3] = 30; // won't do anything because there isn't space in the view for it.
-console.log(view);
-console.log(ab);
+greet(function (data) {
+   console.log("Callback called with data: " + data);
+})
