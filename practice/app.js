20345fd132baafbed176655c0763da21b3bec155
Tom Hoffman <thomas.hoffman@infinite.com>
2019-10-06 19:51:23 -0500

    Classes, Extending with EventEmitter

--- practice/app.js
+++ practice/app.js
@@ -1,19 +1,9 @@
 'use strict';
 
-// classes are really just syntactic sugar for hiding prototypal inheritance. 
-class Person {
-   constructor(firstname, lastname) {
-      this.firstname = firstname;
-      this.lastname = lastname;
-   }
+var Greetr = require('./greeter');
 
-   greet() {
-      console.log(`Hello ${this.firstname} ${this.lastname}`)
-   }
-}
-
-
-let tom = new Person("Tom", "Hoffman");
-tom.greet();
-
-console.log(tom.__proto__);
+var g = new Greetr();
+g.on('greet', function (data) {
+   console.log("On popped: " + data);
+});
+g.greet("****************************");
