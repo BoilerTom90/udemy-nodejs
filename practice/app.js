db1cb461cf547837245c0622db4806f54fb22af0
Tom Hoffman <thomas.hoffman@infinite.com>
2019-10-06 19:30:59 -0500

    Classes and Inheritcance Prototype

--- practice/app.js
+++ practice/app.js
@@ -1,25 +1,19 @@
-var EventEmitter = require('events');
-var util = require('util');
+'use strict';
 
+// classes are really just syntactic sugar for hiding prototypal inheritance. 
+class Person {
+   constructor(firstname, lastname) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+   }
 
-function Greetr() {
-   this.greeting = "Hello World";
+   greet() {
+      console.log(`Hello ${this.firstname} ${this.lastname}`)
+   }
 }
 
-// modify Greetr so that it has all the methods and properties of EventEmitter
-// this must be done before adding to the prototype with other properties/methods.
-// creates a new prototype chain from Greetr to EventEmitter so that Greetr can use EventEmitter directly
-util.inherits(Greetr, EventEmitter);
 
-Greetr.prototype.greet = function (data) {
-   console.log(this.greeting + ": " + data);
-   this.emit('greet', data); // a property from EventEmitter;
-}
+let tom = new Person("Tom", "Hoffman");
+tom.greet();
 
-var g1 = new Greetr();
-
-g1.on('greet', function (data) {
-   console.log("Someone greeted me with data: " + data);
-});
-
-g1.greet("fubar");
+console.log(tom.__proto__);
